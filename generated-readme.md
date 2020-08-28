97f53bfdeeeb4f66b133fd5a665b5966a3540082
Plover Brown <rellwoos@gmail.com>
2020-08-27 18:14:30 -0700

    beginning the process of turning answers into a markdown file

---
+++ generated-readme.md
@@ -0,0 +1,43 @@
+# ${name}
+
+![screenshot of ${name}](${screenshotPath})
+${link}
+
+
+Table of Contents:
+
+- [Section 1: Introduction](#introduction)
+- [Section 2: Usage](#usage)
+- [Section 3: Credits](#credits)
+- [Section 4: License](#license)
+
+## Introduction ##
+
+${description}
+
+## Usage ##
+
+The source files for this quiz can be accessed through cloning the GitHub repo. The link above will take you to the deployed project.
+
+## Credits ##
+
+Thanks to my coding co-op, and you can catch some of 'em here:
+
+- [Zachary Bryan](https://github.com/zacharybryan)
+- [Tim Sanders](https://github.com/tbsanders5)
+- [Nick Konzen](https://github.com/NTKonzen)
+- [Kira Bismuth Lowrey](https://github.com/KILowrey)
+- [Nancy Lambert-Brown](https://github.com/n-lambert)
+- [Mando Estrada](https://github.com/Mando619)
+- [Talia Vazquez](https://github.com/taliavazquez)
+- [Perry Theobald](https://github.com/perrytjr)
+- [Jeneth Diestra](https://github.com/jen6one9)
+- [Matthew Rogers](https://github.com/Rogers-Development-Services)
+- [Jefrrey Choi](https://github.com/jepoy92)
+- [Clyde Baron Rapinan](https://github.com/clydebaron2000)
+- [Johnny Lieu](https://github.com/johnnylieu)
+- [Robert Graham](https://github.com/Robmgraham)
+
+## License ##
+
+GNU General Public License
--- index.js
+++ index.js
@@ -1,14 +1,131 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-];
+
+
+
+//ask user questions
+
+async function askQuestions() {
+    try {
+
+        const { name } = await inquirer.prompt({
+        message: 'The title of your project: ',
+        name: 'name',
+        });
+
+        const { description } = await inquirer.prompt({
+        message: 'The description of your project: ',
+        name: 'description',
+        });
+
+        const { link } = await inquirer.prompt({
+        message: 'URL to link to your deployed project: ',
+        name: 'link',
+        });
+
+        const { screenshotPath } = await inquirer.prompt({
+        message: 'File path to screenshot of your project',
+        name: 'screenshotPath',
+        });
+
+        const { installation } = await inquirer.prompt({
+        message: 'Instructions for the installation of your project: ',
+        name: 'installation',
+        });
+
+        const { usage } = await inquirer.prompt({
+        message: 'Instructions for the usage of your project: ',
+        name: 'usage',
+        });
+
+        const { license } = await inquirer.prompt({
+        message: 'What license is your project under? ',
+        name: 'license',
+        });
+
+        let { anycredits } = await inquirer.prompt({
+        type: 'confirm',
+        message: 'Is there anyone that you would like to credit for contributing to your project? ',
+        name: 'anycredits',
+        default: true,
+        });
+
+        let listOfCredits = []
+        while (anycredits) {   
+            const { credits } = await inquirer.prompt({
+                message: 'Name of person you would like to credit: ',
+                name: 'credits',
+                });
+
+            listOfCredits.push(credits);
+
+            const { morecredits } = await inquirer.prompt({
+                type: 'confirm',
+                message: 'Is there anyone else that you would like to credit for contributing to your project? ',
+                name: 'morecredits',
+                default: true,
+                });
+
+            if (!morecredits) {
+                anycredits = false;
+            }
+        }  
+
+        console.log(listOfCredits);
+        const { contributing } = await inquirer.prompt({
+        message: 'Instructions for those who might contribute to your project: ',
+        name: 'contributing',
+        });
+
+        const { tests } = await inquirer.prompt({
+        message: 'What tests exist for your project? ',
+        name: 'tests',
+        });
+
+        const { questions } = await inquirer.prompt({
+        message: 'What questions remain for your project? ',
+        name: 'questions',
+        });
+
+        answers = {
+            name: name,
+            description: description,
+            link: link,
+            screenshotPath: screenshotPath,
+            installation: installation,
+            usage: usage,
+            license: license,
+            listOfCredits: listOfCredits,
+            contributing: contributing,
+            tests: tests,
+            questions: questions,
+        }
+        console.log(answers);
+        return answers;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+
+    const createdHTML = '';
+    fs.writeFile('README.md', createdHTML, function(err){
+        if (err) {
+            console.log(err);
+        }
+    })
 }
 
 // function to initialize program
 function init() {
+    const answers = askQuestions();
+
+    writeToFile(answers);
 
 }
 
